6ea3056a4d56752c41648770d55b895b2116a983
BabyGecko0 <malasialeks1@gmail.com>
2024-01-22 14:33:01 +0100

    newcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+
+  //Validime per FORMBUILDER SIPAS KERKESES//
+  form: FormGroup;
+
+
+ constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      number: ['', Validators.required],
+      birthday: ['', Validators.required]
+    });
+ }
+
+  
+
+
+
 }
